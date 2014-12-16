a4e4b1ddb7e3e687c1ba0dc47f23063d2e7d07b9
Ting-Yen Lai <banacorn@gmail.com>
2014-12-16 13:13:40 +0800

    factoring Pipeline stuffs out

--- src/Compiler/Type.hs
+++ src/Compiler/Type.hs
@@ -1,44 +1,9 @@
 module Compiler.Type
     (   module Compiler.Type.AST
     ,   module Compiler.Type.Scope
-    ,   Pipeline(..)
-    ,   Position(..)
-    ,   PipelineError(..)
+    ,   module Compiler.Type.Pipeline
     ) where
-
-import Control.Monad.Except
-import Data.List (intercalate)
 
 import Compiler.Type.Scope
 import Compiler.Type.AST
-
--- Pipeline
-type Pipeline = ExceptT PipelineError IO
-
-data Position = Position {
-        posOffset :: Int
-    ,   posLine :: Int
-    ,   posColumn :: Int
-    }
-    deriving (Eq)
-
-instance Show Position where
-    show (Position offset line column) = "Position " ++ show offset ++ " " ++ show line ++ " " ++ show column
---         "@ " ++ show offset
---         ++ " L " ++ show line
---         ++ " C " ++ show column
-
-data PipelineError  = FileError String
-                    | LexError Position String
-                    | ParseError Position String
-                    | SemanticError String
-                    deriving (Eq)
-instance Show PipelineError where
-    show (FileError e) = e
-    show (LexError pos msg) = "Lex Error: \n"
-        ++ show pos ++ "\n"
-        ++ msg
-    show (ParseError pos msg) = "Lex Error: \n"
-        ++ show pos ++ "\n"
-        ++ msg
-    show (SemanticError e) = e
+import Compiler.Type.Pipeline
---
+++ src/Compiler/Type/Pipeline.hs
@@ -0,0 +1,34 @@
+module Compiler.Type.Pipeline where
+
+import Control.Monad.Except
+
+-- Pipeline
+type Pipeline = ExceptT PipelineError IO
+
+data Position = Position {
+        posOffset :: Int
+    ,   posLine :: Int
+    ,   posColumn :: Int
+    }
+    deriving (Eq)
+
+instance Show Position where
+    show (Position offset line column) = "Position " ++ show offset ++ " " ++ show line ++ " " ++ show column
+--         "@ " ++ show offset
+--         ++ " L " ++ show line
+--         ++ " C " ++ show column
+
+data PipelineError  = FileError String
+                    | LexError Position String
+                    | ParseError Position String
+                    | SemanticError String
+                    deriving (Eq)
+instance Show PipelineError where
+    show (FileError e) = e
+    show (LexError pos msg) = "Lex Error: \n"
+        ++ show pos ++ "\n"
+        ++ msg
+    show (ParseError pos msg) = "Lex Error: \n"
+        ++ show pos ++ "\n"
+        ++ msg
+    show (SemanticError e) = e
