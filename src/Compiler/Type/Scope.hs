d1fc9120b564c2d1e6f0f2301921e718c3d7a6cf
Ting-Yen Lai <banacorn@gmail.com>
2015-01-05 14:05:02 +0800

    Type of Symbols

--- src/Compiler/Type/Scope.hs
+++ src/Compiler/Type/Scope.hs
@@ -2,6 +2,14 @@ module Compiler.Type.Scope where
 
 type Depth = Int
 
+data FOType = TypeInteger
+            | TypeReal
+            | TypeString
+            | TypeArray FOType
+
+data HOType = FunctionType [FOType] FOType
+            | ProcedureType [FOType]
+
 data Symbol = Declared String
             | Used String
             deriving (Eq, Show)
