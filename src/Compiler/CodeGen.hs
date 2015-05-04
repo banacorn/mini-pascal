a94abb0213521ac0a78a81280aa972473559c377
Ting-Yen Lai <banacorn@gmail.com>
2015-05-05 00:35:42 +0800

    global variable declaration

--- src/Compiler/CodeGen.hs
+++ src/Compiler/CodeGen.hs
@@ -2,24 +2,27 @@ module Compiler.CodeGen where
 
 -- import Control.Monad.Except
 import Compiler.Type.Pipeline
-import Compiler.AST.Type
+import qualified Compiler.AST.Type as AST
+import Compiler.AST.Type hiding (Type)
 
 import LLVM.General.AST
 import LLVM.General.AST.Type
 import LLVM.General.AST.Global as Glb
 
-toModule :: ABT -> Module
-toModule _ = defaultModule {
-        moduleName = "program"
-    ,   moduleDefinitions = [
-            GlobalDefinition $ globalVariableDefaults {
-                    Glb.name = Name "banana"
-                ,   Glb.type' = i32
-                }
-        ]
+genType :: AST.Type -> Type
+genType (BasicType IntType) = i32
+genType (BasicType RealType) = double
+genType (BasicType AST.VoidType) = void
+genType (AST.FunctionType _) = error "can't gen higher order type"
+
+genGlobalVariable :: Declaration -> Definition
+genGlobalVariable (Declaration (Symbol name _) typ) = GlobalDefinition $ globalVariableDefaults {
+        Glb.name = Name name
+    ,   Glb.type' = genType typ
     }
 
---
---
--- genGlobalVariable :: ABT -> [GlobalDefinition]
--- genGlobalVariable (Program decs _)
+toModule :: ABT -> Module
+toModule (Program decs _ _) = defaultModule {
+        moduleName = "program"
+    ,   moduleDefinitions = [ genGlobalVariable dec | dec <- decs, not (isFunction (decType dec)) ] 
+    }
